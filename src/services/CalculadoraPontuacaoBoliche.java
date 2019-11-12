21b4ba02968a343b492967a4c3526ad7a03c78b7
Diego Mucheniski <mucheniski@gmail.com>
2019-11-12 20:33:00 -0300

    Finalizado com todos os testes ok

--- src/services/CalculadoraPontuacaoBoliche.java
+++ src/services/CalculadoraPontuacaoBoliche.java
@@ -7,15 +7,63 @@ public class CalculadoraPontuacaoBoliche {
 	
 	public int pontuacaoDoJogo(int[] jogadas) { 
 				
-		int somaPontos = 0;
-		int qteJogadas = jogadas.length;
+		int somaPontos = 0;		
+		int rodada = 0;
+		int contaJogadas = 0;
 		
-		for (int i = 0; i < qteJogadas; i++) {
+		for (int i = 0; i < jogadas.length; i++) {
 			somaPontos += jogadas[i];
+			contaJogadas++;
+			if (isStrike(jogadas[i])) {
+				rodada++;
+				if(isUltimaRodada(rodada)) {
+					somaPontos += bonusStrike(jogadas[i+1], jogadas[i+2]);
+					return somaPontos;
+				}
+				else {
+					somaPontos += bonusStrike(jogadas[i+1], jogadas[i+2]);
+				}
+				contaJogadas = 0;
+			}
+			
+			if (contaJogadas == 2) {
+				rodada++;
+				if (isSpare(jogadas[i], jogadas[i-1])) {
+					if (isUltimaRodada(rodada)) {
+						somaPontos += bonusSpare(jogadas[i+1]);
+						return somaPontos;
+					}
+					else {
+						somaPontos += bonusSpare(jogadas[i+1]);
+					}
+				}
+				contaJogadas = 0;
+			}
+			
 		}
 		
 		return somaPontos;
 		
+	}
+	
+	private Boolean isStrike(int pontos) {
+		return pontos == STRIKE;
+	}
+	
+	private Boolean isSpare(int jogadaAtual, int jogadaAnterior) {
+		return (jogadaAtual + jogadaAnterior) == STRIKE;
+	}
+	
+	private Boolean isUltimaRodada(int rodada) {
+		return rodada == TOTALRODADAS;
+	}
+	
+	private int bonusStrike(int jogada1, int jogada2) {
+		return jogada1 + jogada2;
+	}
+	
+	private int bonusSpare(int jogada) {
+		return jogada;
 	}
 
 }
--- src/services/CalculadoraTest.java
+++ src/services/CalculadoraTest.java
@@ -7,10 +7,34 @@ class CalculadoraTest {
 
 	CalculadoraPontuacaoBoliche calculadora = new CalculadoraPontuacaoBoliche();	
 	int[] jogoSemStrikeOuSpare = {0,0, 0,0, 0,0, 0,0, 0,0, 0,0, 0,0, 1,2, 3,0, 0,0};
+	int[] jogoComStrike = {0,0, 0,0, 0,0, 0,0, 0,0, 0,0, 0,0, 10, 2,3, 0,0};
+	int[] jogoComSpare = {0,0, 0,0, 0,0, 0,0, 0,0, 0,0, 0,0, 2,8, 2,3, 0,0};
+	int[] jogoComSpareEStrike = {1,4, 4,5, 6,4, 5,5, 10, 0,1, 7,3, 6,4, 10, 2,8, 6};
+	int[] jogoPerfeito = {10 , 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10};
 	
 	@Test
 	void jogoSemStrikeOuSpareTest() {
 		assertEquals(6, resultadoJogo(jogoSemStrikeOuSpare));
+	}
+	
+	@Test
+	void jogoComStrikeTest() {
+		assertEquals(20, resultadoJogo(jogoComStrike));
+	}
+	
+	@Test
+	void jogoComSpareTest() {
+		assertEquals(17, resultadoJogo(jogoComSpare));
+	}
+	
+	@Test
+	void jogoComSpareEStrikeTest() {
+		assertEquals(133, resultadoJogo(jogoComSpareEStrike));
+	}
+	
+	@Test
+	void jogoPerfeitoTest() {
+		assertEquals(300, resultadoJogo(jogoPerfeito));
 	}
 	
 	private int resultadoJogo(int[] jogadas) {
