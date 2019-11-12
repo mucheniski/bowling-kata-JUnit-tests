951eb4ce064e768515d057a717e532451990967f
Diego Mucheniski <mucheniski@gmail.com>
2019-11-12 19:25:25 -0300

    jogoSemStrikeOuSpareTest

--- src/services/CalculadoraPontuacaoBoliche.java
+++ src/services/CalculadoraPontuacaoBoliche.java
@@ -2,8 +2,20 @@ package services;
 
 public class CalculadoraPontuacaoBoliche {		
 	
+	private static final int STRIKE = 10;
+	private static final int TOTALRODADAS = 10;
+	
 	public int pontuacaoDoJogo(int[] jogadas) { 
-		return 0;
+				
+		int somaPontos = 0;
+		int qteJogadas = jogadas.length;
+		
+		for (int i = 0; i < qteJogadas; i++) {
+			somaPontos += jogadas[i];
+		}
+		
+		return somaPontos;
+		
 	}
 
 }
--- src/services/CalculadoraTest.java
+++ src/services/CalculadoraTest.java
@@ -5,6 +5,16 @@ import org.junit.jupiter.api.Test;
 
 class CalculadoraTest {
 
+	CalculadoraPontuacaoBoliche calculadora = new CalculadoraPontuacaoBoliche();	
+	int[] jogoSemStrikeOuSpare = {0,0, 0,0, 0,0, 0,0, 0,0, 0,0, 0,0, 1,2, 3,0, 0,0};
 	
+	@Test
+	void jogoSemStrikeOuSpareTest() {
+		assertEquals(6, resultadoJogo(jogoSemStrikeOuSpare));
+	}
+	
+	private int resultadoJogo(int[] jogadas) {
+		return calculadora.pontuacaoDoJogo(jogadas); 
+	}	
 
 }
